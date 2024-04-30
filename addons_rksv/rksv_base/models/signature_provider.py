5206996d2dbc31113dd95fd0f0b12d8b056b57ab
Wolfgang Pichler (Callino) <wpichler@callino.at>
2024-04-30 12:07:51 +0200

    [UPD] Code cleanup.

--- addons_rksv/rksv_base/models/signature_provider.py
+++ addons_rksv/rksv_base/models/signature_provider.py
@@ -29,7 +29,7 @@ class SignatureProvider(models.Model):
         string="Status",
         readonly=True,
         default='UNBEKANNT',
-        track_visibility='onchange',
+        tracking=True,
         copy=False
     )
     bmf_last_update = fields.Datetime(
@@ -37,6 +37,6 @@ class SignatureProvider(models.Model):
         copy=False)
     bmf_message = fields.Char(
         string="BMF Status Text",
-        track_visibility='onchange',
+        tracking=True,
         copy=False
     )
